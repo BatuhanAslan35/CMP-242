7685c17c75fcf9f1ab1e0c55c1c39736a801a6af
Batuhan <k20240995@std.kyrenia.edu.tr>
2025-12-16 14:14:32 +0200

    Add files via upload

---
+++ FlowSense/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flowsense",
+    version="1.0.0",
+    description="Intelligent Traffic Monitoring and Alert System Simulation",
+    author="Batuhan Aslan",
+    author_email="k20240995@std.kyrenia.edu.tr",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
